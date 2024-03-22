69ee0b5c7eab2c792056320331aed57b014b18a6
jcblanc2 <jclaytonblanc@gmail.com>
2024-03-22 00:14:35 -0400

    chore(fix): refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
