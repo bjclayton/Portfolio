e8cf8c9c22d92894193dd6c91be0a474db66a779
John Clayton Blanc <108766949+jcblanc2@users.noreply.github.com>
2024-05-03 10:54:15 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
     origin: [
-        "http://localhost:5173",
+        "https://www.johnclaytonblanc.com",
         "https://johnclayton.vercel.app"
     ]
 }));
